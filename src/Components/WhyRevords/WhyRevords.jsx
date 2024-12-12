1f24b4d7eec5759e4170a1e8dc8a06097e901a78
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-12-12 17:41:33 +0530

    Icons Added

--- src/Components/WhyRevords/WhyRevords.jsx
+++ src/Components/WhyRevords/WhyRevords.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import "../../Styles/WhyRevords.css"
 import { Row, Col } from "antd";
+import MultipleWays from "/Icons/MultipleWays.png"
+import visitingandspendinghabits from "/Icons/visitingandspendinghabits.png"
+import Customizablelook from "/Icons/Customizablelook.png"
+import Powerfulandaccurate from "/Icons/Powerfulandaccurate.png"
+import Managemultiplelocations from "/Icons/Managemultiplelocations.png"
+import Simplifiedcustomer from "/Icons/Simplifiedcustomer.png"
 const WhyRevords = () => {
 
     const CardsContent = [
         {
-            title: "Simplified customer enrollment"
+            title: "Simplified customer enrollment",
+            image:Simplifiedcustomer
         },
         {
-            title: "Multiple ways of earning points for customers"
+            title: "Multiple ways of earning points for customers",
+            image: MultipleWays
         },
         {
-            title: "Tag your customers by their visiting and spending habits"
+            title: "Tag your customers by their visiting and spending habits",
+            image: visitingandspendinghabits
         },
         {
-            title: "Customizable look and feel for different businesses"
+            title: "Customizable look and feel for different businesses",
+            image: Customizablelook
         },
         {
-            title: "Manage multiple locations effectively"
+            title: "Manage multiple locations effectively",
+            image: Managemultiplelocations
         },
         {
-            title: "Powerful and accurate analytics"
+            title: "Powerful and accurate analytics",
+            image: Powerfulandaccurate
         }
     ]
     return (
@@ -33,11 +45,11 @@ const WhyRevords = () => {
                     <div>
                         <Row>
                             {CardsContent.map((item, index) => (
-                                <Col key={index} lg={8} md={12} style={{width:"100%"}}>
+                                <Col key={index} lg={8} md={12} style={{ width: "100%" }}>
                                     <div className="AnimatedCardContainer">
                                         <span>0{index + 1}</span>
-                                        <div>
-
+                                        <div className="CardIconContainer">
+                                            <img src={item.image} alt="" />
                                         </div>
                                         <div className="HeadingContainer">
                                             <h4>{item.title}</h4>
