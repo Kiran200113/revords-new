ca69ded9ba2ba25219132e07d38c45f3baf12132
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-12-17 13:29:28 +0530

    Update Client and location section

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import "../../Styles/Clients.css";
-import { Row, Col } from 'antd';
+import { Row, Col,Image } from 'antd';
 const Clients = () => {
     return (
         <>
-            <section id="OurClients" className="SectionStyle">
-                <h2 className="sectionHeading">Our Clients</h2>
+            <section id="OurClients" >
+
                 <Row>
-                    <Col lg={1} md={0} xs={0} sm={0} />
-                    <Col lg={10} md={12} xs={24} sm={24}>
-                        <div className="ClientCardContainer">
-                            <img src="/Images/Elite Gaming.png" />
+
+                    <Col lg={12} md={12} xs={24} sm={24}>
+                        <div>
+                            <h2 className="sectionHeading">Our Location</h2>
+                            <div className="LocationImage">
+                                <Image src="/Images/RevordsLocationMap.png" alt="" />
+                            </div>
                         </div>
                     </Col>
-                    <Col lg={1} md={0} xs={0} sm={0} />
-                    <Col lg={10} md={12} xs={24} sm={24}>
-                        <div className="ClientCardContainer">
-                            <img src="/Images/LuckyDay.png" />
+                    <Col lg={12} md={12} xs={24} sm={24}>
+                        <div className="SecondColumn">
+                            <h2 className="sectionHeading">Our Clients</h2>
+                            <div className="ClientCardContainer">
+                                <img src="/Images/Elite Gaming.png" />
+                                <img src="/Images/LuckyDay.png" />
+                            </div>
                         </div>
                     </Col>
                 </Row>
+
             </section>
         </>
     )
---
+++ src/Components/OurLocation/RevordsLocation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RevordsLocation = () => {
+    return (
+        <>
+            <section id="RevordsLocationContainer">
+                <div className="RevordsLocationContainer">
+
+                </div>
+            </section>
+        </>
+    )
+}
+export default RevordsLocation
