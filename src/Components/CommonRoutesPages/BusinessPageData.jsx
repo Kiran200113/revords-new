ce7cabeac0e279302339382d63c9e1d5bbbf0140
Megh39 <148150265+webtesting22@users.noreply.github.com>
2024-12-18 17:38:53 +0530

    Update BusinessPageData.jsx

--- src/Components/CommonRoutesPages/BusinessPageData.jsx
+++ src/Components/CommonRoutesPages/BusinessPageData.jsx
@@ -2,29 +2,30 @@ import { Link } from "react-router-dom";
 
 export const BusinessPageData = [
     {
-        slug: 'hotspotgaming',
-        bannertitle: 'Hot spot Gaming',
+        slug: 'hotspotcafe',
+        bannertitle: 'Hot spot',
         bannerTagline: 'Cafe & Lounge',
         businessLogo: "/Images/Hot Spot Logo.png",
         bannerImage: "https://images.unsplash.com/photo-1644461150893-19421f5020d8?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        mainheading: "About Hot spot gaming cafe",
+        mainheading: "About Hot spot cafe",
         businessImages: [
             "/Images/Hot Spot Cafe - inside.png",
             "/Images/Hot Spot Cafe - outside ( Copy ).jpg",
         ],
         descriptionData: <>
-            <p>Hot Spot Gaming Cafe is a warm and friendly gaming venue providing a comfortable and secure environment where guests can relax, enjoy drinks, and experience the latest in interactive entertainment.</p>
+            <p>Hot Spot Cafe is a warm and friendly venue that provides a cozy space for guests to relax, savor drinks, and enjoy a selection of light snacks and food. It’s the ideal spot to hang out, socialize, and experience a lively atmosphere with friends.
+            </p>
             <p><b>Address:&nbsp;</b>4609 S Harlem Ave, Unit A, Forest View, IL 60402</p>
             <p><b>Phone:</b><Link to="tel:7737169873">&nbsp;(773)-716-9873</Link></p>
             <p><b>Hours:</b></p>
             <ul>
-                <li>Monday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Tuesday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Wednesday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Thursday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Friday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Saturday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Sunday:&nbsp;9:00AM to 2:00AM</li>
+                <li>Monday:&nbsp;09:00AM to 02:00AM</li>
+                <li>Tuesday:&nbsp;09:00AM to 02:00AM</li>
+                <li>Wednesday:&nbsp;09:00AM to 02:00AM</li>
+                <li>Thursday:&nbsp;09:00AM to 02:00AM</li>
+                <li>Friday:&nbsp;09:00AM to 02:00AM</li>
+                <li>Saturday:&nbsp;09:00AM to 02:00AM</li>
+                <li>Sunday:&nbsp;09:00AM to 02:00AM</li>
             </ul>
             <div>
                 <p><b>Legal</b></p>
@@ -63,7 +64,7 @@ export const BusinessPageData = [
                 <li>Thursday:&nbsp;Closed</li>
                 <li>Friday:&nbsp;11:00AM to 10:00PM</li>
                 <li>Saturday:&nbsp;11:00AM to 10:00PM</li>
-                <li>Sunday:&nbsp;11:00AM to 9PM</li>
+                <li>Sunday:&nbsp;11:00AM to 09:00PM</li>
             </ul>
             <div>
                 <p><b>Legal</b></p>
@@ -97,13 +98,13 @@ export const BusinessPageData = [
             <p><b>Phone:</b><Link to="tel:7085460877">&nbsp;(708)-546-0877</Link></p>
             <p><b>Hours:</b></p>
             <ul>
-                <li>Monday:&nbsp;2:00PM to 2:00AM</li>
-                <li>Tuesday:&nbsp;2:00PM to 2:00AM</li>
-                <li>Wednesday:&nbsp;2:00PM to 2:00AM</li>
-                <li>Thursday:&nbsp;2:00PM to 2:00AM</li>
-                <li>Friday:&nbsp;2:00PM to 2:00AM</li>
-                <li>Saturday:&nbsp;2:00PM to 2:00AM</li>
-                <li>Sunday:&nbsp;2:00PM to 2:00AM</li>
+                <li>Monday:&nbsp;02:00PM to 02:00AM</li>
+                <li>Tuesday:&nbsp;02:00PM to 02:00AM</li>
+                <li>Wednesday:&nbsp;02:00PM to 02:00AM</li>
+                <li>Thursday:&nbsp;02:00PM to 02:00AM</li>
+                <li>Friday:&nbsp;02:00PM to 02:00AM</li>
+                <li>Saturday:&nbsp;02:00PM to 02:00AM</li>
+                <li>Sunday:&nbsp;02:00PM to 02:00AM</li>
             </ul>
             <div>
                 <p><b>Legal</b></p>
@@ -138,13 +139,13 @@ export const BusinessPageData = [
             <p><b>Phone:</b><Link to="tel:7737169873">&nbsp;(773)-716-9873</Link></p>
             <p><b>Hours:</b></p>
             <ul>
-                <li>Monday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Tuesday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Wednesday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Thursday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Friday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Saturday:&nbsp;9:00AM to 2:00AM</li>
-                <li>Sunday:&nbsp;9:00AM to 2:00AM</li>
+                <li>Monday:&nbsp;09:00AM to 2:00AM</li>
+                <li>Tuesday:&nbsp;09:00AM to 2:00AM</li>
+                <li>Wednesday:&nbsp;09:00AM to 2:00AM</li>
+                <li>Thursday:&nbsp;09:00AM to 2:00AM</li>
+                <li>Friday:&nbsp;09:00AM to 2:00AM</li>
+                <li>Saturday:&nbsp;09:00AM to 2:00AM</li>
+                <li>Sunday:&nbsp;09:00AM to 2:00AM</li>
             </ul>
             <div>
                 <p><b>Legal</b></p>
